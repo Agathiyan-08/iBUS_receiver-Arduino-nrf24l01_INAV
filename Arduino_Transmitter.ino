5fd385c3d8d11072f0588c5516cfa4f44d67031a
Aga-08 <166924988+Agathiyan-08@users.noreply.github.com>
2025-11-10 11:16:55 +0530

    Update Arduino_Transmitter.ino

--- Arduino_Transmitter.ino
+++ Arduino_Transmitter.ino
@@ -1,26 +1,20 @@
 #include <SPI.h>
 #include <RF24.h>
-#include <SoftwareSerial.h>
 
 RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "00001";
 
-// Software Serial for IBUS (connect to flight controller's RX)
-SoftwareSerial ibusSerial(3, 4); 
-
 uint16_t channels[14] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
 
-unsigned long lastReceiveTime = 0;
-const unsigned long connectionTimeout = 1000;
-bool isConnected = false;
-const int statusLedPin = 2;
+const int potPins[6] = {A0, A1, A2, A3, A4, A5}; // inputs 
 
-uint8_t ibusPacket[32];
+unsigned long lastSendTime = 0;
+bool connectionLedState = false;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("IBUS Receiver - Starting");
- 
+  Serial.println("Transmitter - Starting");
+
   if (!radio.begin()) {
     Serial.println("nRF24L01 initialization FAILED!");
     while (1) {}
@@ -28,83 +22,62 @@ void setup() {
   
   Serial.println("nRF24L01 initialized successfully");
   
-  radio.openReadingPipe(0, address);
+  radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.setDataRate(RF24_250KBPS);
   radio.setRetries(15, 15);
-  radio.startListening();
+  radio.stopListening();
 
-  ibusSerial.begin(115200);
+  for (int i = 0; i < 6; i++) {
+    pinMode(potPins[i], INPUT);
+  }
 
-  pinMode(statusLedPin, OUTPUT);
-  digitalWrite(statusLedPin, LOW);
-
-  initializeIbusPacket();
+  pinMode(2, INPUT_PULLUP); // Switch 1
+  pinMode(3, INPUT_PULLUP); // Switch 2
+  pinMode(4, INPUT_PULLUP); // Switch 3
   
-  Serial.println("IBUS Receiver Ready - Waiting for transmitter");
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("Transmitter Ready");
 }
 
 void loop() {
-  if (radio.available()) {
-    uint16_t receivedChannels[14];
-    radio.read(&receivedChannels, sizeof(receivedChannels));
-    
-    for (int i = 0; i < 14; i++) {
-      channels[i] = receivedChannels[i];
+  readInputs();
+  bool success = radio.write(&channels, sizeof(channels));
+  
+  if (success) {
+    if (millis() - lastSendTime > 500) {
+      connectionLedState = !connectionLedState;
+      digitalWrite(LED_BUILTIN, connectionLedState);
+      lastSendTime = millis();
     }
-    
-    lastReceiveTime = millis();
-    
-    if (!isConnected) {
-      isConnected = true;
-      Serial.println("Transmitter CONNECTED!");
-      digitalWrite(statusLedPin, HIGH);
-    }
+  } else {
+    Serial.println("Send failed!");
+    digitalWrite(LED_BUILTIN, LOW);
   }
   
-  sendIbusToFC();
-  checkConnection();
-  
-  delay(10);
-}
-
-void initializeIbusPacket() {
-  ibusPacket[0] = 0x20; 
-  ibusPacket[1] = 0x40; 
-}
-
-void sendIbusToFC() {
-  for (int i = 0; i < 14; i++) {
-    ibusPacket[2 + i * 2] = channels[i] & 0xFF;       
-    ibusPacket[3 + i * 2] = (channels[i] >> 8) & 0xFF; 
+  static unsigned long lastPrintTime = 0;
+  if (millis() - lastPrintTime > 500) {
+    Serial.print("Ch1(Roll):"); Serial.print(channels[0]);
+    Serial.print(" Ch2(Pitch):"); Serial.print(channels[1]);
+    Serial.print(" Ch3(Throttle):"); Serial.print(channels[2]);
+    Serial.print(" Ch4(Yaw):"); Serial.println(channels[3]);
+    lastPrintTime = millis();
   }
-
-  uint16_t checksum = 0xFFFF;
-  for (int i = 0; i < 30; i++) {
-    checksum -= ibusPacket[i];
-  }
-  ibusPacket[30] = checksum & 0xFF;        
-  ibusPacket[31] = (checksum >> 8) & 0xFF; 
-
-  ibusSerial.write(ibusPacket, 32);
+  
+  delay(20); 
 }
 
-void checkConnection() {
-  if (millis() - lastReceiveTime > connectionTimeout) {
-    if (isConnected) {
-      isConnected = false;
-      Serial.println("Transmitter DISCONNECTED!");
-      digitalWrite(statusLedPin, LOW);
-      
-      for (int i = 0; i < 14; i++) {
-        channels[i] = 1000; 
-      }
-    }
+void readInputs() {
+  channels[0] = map(analogRead(potPins[1]), 0, 1023, 1000, 2000); // Roll (A0)
+  channels[1] = map(analogRead(potPins[0]), 0, 1023, 1000, 2000); // Pitch (A1)
+  channels[2] = map(analogRead(potPins[2]), 0, 1023, 1000, 2000); // Throttle (A2)
+  channels[3] = map(analogRead(potPins[3]), 0, 1023, 1000, 2000); // Yaw (A3)
+
+  channels[4] = digitalRead(2) ? 1000 : 2000; // Switch 1
+  channels[5] = digitalRead(3) ? 1000 : 2000; // Switch 2
+  channels[6] = digitalRead(4) ? 1000 : 2000; // Switch 3
   
-    static unsigned long lastBlinkTime = 0;
-    if (millis() - lastBlinkTime > 500) {
-      digitalWrite(statusLedPin, !digitalRead(statusLedPin));
-      lastBlinkTime = millis();
-    }
+  for (int i = 7; i < 14; i++) {
+    channels[i] = 1500;
   }
 }
